d0b1fd81591221058cdee7fe07c2ed15c6e67011
= <tigran_grigoryan05@inbox.ru>
2023-04-15 12:08:53 +0400

    Update

--- src/main/java/com/airplane/App.java
+++ src/main/java/com/airplane/App.java
@@ -1,18 +1,12 @@
 package com.airplane;
 
-import com.airplane.model.Passenger;
-import com.airplane.model.Trip;
-import com.airplane.service.CompanyService;
-import com.airplane.service.PassengerService;
 
 public class App 
 {
+	
     public static void main(String[] args)
     {
-    	CompanyService cs = new CompanyService();
-    	
-    	System.out.println(cs.getById(1));
-    	
+    
     	
     }
 }
